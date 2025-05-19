1399614ee0e5492276944cecdb0dfcb0d14771ab
Obiri254 <anthonymburu4449@gmail.com>
2025-05-19 05:09:08 +0300

    first commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import './Hero.css'
-import cropped_image from '../../assets/cropped_image.png'
+// import zebra from '../../assets/zebra.jpg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
   return (
     <div id='home' className='hero'>
-      <img src={cropped_image} alt="" />
-      <h1><span>I'm Obiri Anthony,</span> a backend developer based in Kenya.</h1>
-      <p>I am a backend and frontend developer from Nakuru, Kenya with experince in developing server side systems using javascript</p>
+      <div className='background'>
+      {/* <img src={zebra} alt="" /> */}
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Let's Connect</AnchorLink></div>
         <div className="hero-resume">My resume</div>
+      </div>
+      <h1>Ally Safaris</h1>
+      <p>Ally Safaris, offers exceptional safari experiences focused on Kenya’s renowned wildlife. Specializing in guided tours we take guests through iconic locations like the Maasai Mara, Lake Nakuru, Amboseli, Samburu, Ol Pejeta and Many More.</p>
       </div>
     </div>
   )
